80b8e0ffe4f1650250b5faae773047af96c867c6
WillAnderson1339 <WillAnderson1339@gmail.com>
2024-01-11 15:33:13 -0800

    First implementation of Ladder

--- Level.py
+++ Level.py
@@ -1,7 +1,21 @@
-
 import pygame
+import random
 
 from constants import *
+
+class Ladder(object):
+    def __init__(self, x, y, width, height, colour, direction):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.colour = colour
+        self.direction = direction
+
+    def draw(self, win):
+        if self.direction == 1:
+            pygame.draw.rect(win, self.colour, (self.x, self.y, self.width, self.height), 2)
+
 
 class Level(object):
     def __init__(self, num_floors, num_enemies, colour):
@@ -19,13 +33,33 @@ class Level(object):
             width = WINDOW_WIDTH
             height = 10
             rect = (x, y, width, height)
+            id = i
+            ladders = []
 
-            self.floors.append((colour, rect))
+            self.floors.append((id, colour, rect, ladders))
+
+        self.init_ladders()
+
+    def init_ladders(self):
+        for item in self.floors:
+            if (item[0] == 0):
+                pass
+
+            x = random. randint(10, WINDOW_WIDTH - 150)
+            #x = item[2][0]
+            y = item[2][1]
+            width = 50
+            height = 165
+
+            ladder = Ladder(x, y, width, height, self.colour, 1)
+            item[3].append(ladder)
 
 
     def draw(self, win):
         for item in self.floors:
-            pygame.draw.rect(win, item[0], item[1])
+            pygame.draw.rect(win, item[1], item[2])
+            for ladder in item[3]:
+                ladder.draw(win)
 
     def foo(self):
         if self.num_floors == -1:
